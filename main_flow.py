89fe67fcd94fdb38d370403c195fa2d0909c2809
HaodiFan <haodi.fan@izhijue.com>
2023-12-11 19:23:15 +0800

    added config demo

--- main_flow.py
+++ main_flow.py
@@ -13,7 +13,7 @@ def main():
 
     while 1:
         current_datetime = datetime.now()
-        time_delta = current_datetime - datetime(current_datetime.year, current_datetime.month, current_datetime.day, 1,
+        time_delta = current_datetime - datetime(current_datetime.year, current_datetime.month, cuuurrent_datetime.day, 1,
                                                  0, 0)
         if 5 > time_delta.seconds > 0:
             logger.info(f"Current time: {current_datetime}")
